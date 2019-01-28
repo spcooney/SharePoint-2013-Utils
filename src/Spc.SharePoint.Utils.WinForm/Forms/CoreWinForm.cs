2e77aa4b3a486c68d15d12e642abd77a4b74b0fc
Shawn Cooney <shawn.cooney@treasury.gov>
2019-01-28 10:52:57 -0500

    Creating the base form.

---
+++ src/Spc.SharePoint.Utils.WinForm/Forms/CoreWinForm.cs
@@ -0,0 +1,62 @@
+﻿namespace Spc.SharePoint.Utils.WinForm.Forms
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Windows.Forms;
+
+    internal class CoreWinForm : Form
+    {
+        private IntPtr _handle;
+
+        internal CoreWinForm()
+        {
+        }
+
+        /// <summary>
+        /// Finds all the controls within the form.
+        /// </summary>
+        /// <param name="container">The control to search.</param>
+        /// <param name="controls">The existing list of controls found.</param>
+        /// <returns>A list of controls.</returns>
+        internal List<Control> GetAllControls(Control container, ref List<Control> controls)
+        {
+            foreach (Control ctrl in container.Controls)
+            {
+                if (ctrl is Control)
+                {
+                    controls.Add(ctrl);
+                }
+                if (ctrl.Controls.Count > 0)
+                {
+                    controls = GetAllControls(ctrl, ref controls);
+                }
+            }
+            return controls;
+        }
+
+        protected void EnsureFormFits()
+        {
+            Rectangle rect = Screen.GetWorkingArea(this);
+            if (base.Width > rect.Width)
+            {
+                base.Width = rect.Width;
+            }
+            if (base.Height > rect.Height)
+            {
+                base.Height = rect.Height;
+            }
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            this._handle = base.Handle;
+            base.OnHandleCreated(e);
+        }
+
+        internal IntPtr ThreadSafeHandle
+        {
+            get { return (base.InvokeRequired) ? this._handle : base.Handle; }
+        }
+    }
+}
---
+++ src/Spc.SharePoint.Utils.WinForm/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: log4net.Config.XmlConfigurator]
+[assembly: AssemblyTitle("Spc.SharePoint.Utils.WinForm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Shawn P. Cooney")]
+[assembly: AssemblyProduct("Spc.SharePoint.Utils.WinForm")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3f492c66-149a-4905-9c03-0ab864d1d4d9")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
